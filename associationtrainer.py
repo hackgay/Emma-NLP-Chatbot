63c8c67ebb08af64337695c2f7e9d03263ed9e37
hackgay <133792952+hackgay@users.noreply.github.com>
2020-10-21 07:02:34 +0000

    Exception

--- associationtrainer.py
+++ associationtrainer.py
@@ -15,4 +15,4 @@ def calculate_new_weight(currentWeight):
     if currentWeight == 1:
         currentWeight = 0.999999999994
     
-    # Transform the weight back into t
+    # Transform the weight back into the number of occurances of the
