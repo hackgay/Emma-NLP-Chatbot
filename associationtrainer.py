71cf0341ec7e36397f8eb31d9dd66e536b0c5903
hackgay <133792952+hackgay@users.noreply.github.com>
2020-11-12 05:57:21 +0000

    Updated yarn.lock

--- associationtrainer.py
+++ associationtrainer.py
@@ -35,4 +35,4 @@ def train_association(word, associationType, target):
 
         # Check to see if the association already exists
         with connection:
-          
+            cursor.execute('SELECT * FRO
