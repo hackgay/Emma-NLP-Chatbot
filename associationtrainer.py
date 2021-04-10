c277e110cacfea12ef1f05d0d02cc9b85f5a2d60
hackgay <133792952+hackgay@users.noreply.github.com>
2021-04-10 08:02:09 +0000

    Ignore log files

--- associationtrainer.py
+++ associationtrainer.py
@@ -47,4 +47,5 @@ def train_association(word, associationType, target):
                 # Association does not exist, so add it
                 # This is the weight calculated for all new associations
                 weight = 0.0999999999997
+                with connection:
               
