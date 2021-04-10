df775608b2e02d9b8b75461fe54c76ceb4a24f37
hackgay <133792952+hackgay@users.noreply.github.com>
2021-04-10 19:51:48 +0000

    Reverse geocoding

--- associationtrainer.py
+++ associationtrainer.py
@@ -48,4 +48,4 @@ def train_association(word, associationType, target):
                 # This is the weight calculated for all new associations
                 weight = 0.0999999999997
                 with connection:
-                    cursor.execute('INSERT INTO associatio
+                    cursor.execute('INSERT INTO associationmodel(word, association_t
