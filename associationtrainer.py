a7be02c6e72c952902c2ac8bf8d07b968fd08443
hackgay <133792952+hackgay@users.noreply.github.com>
2021-04-16 16:19:50 +0000

    staging: zram: Add auto loading of module if user opens /dev/zram.

--- associationtrainer.py
+++ associationtrainer.py
@@ -48,4 +48,4 @@ def train_association(word, associationType, target):
                 # This is the weight calculated for all new associations
                 weight = 0.0999999999997
                 with connection:
-                    cursor.execute('INSERT INTO associationmodel(word, association_type, target, weight) VALUES (?, ?, ?, ?);', (word.enco
+                    cursor.execute('INSERT INTO associationmodel(word, association_type, target, weight) VALUES (?, ?, ?, ?);', (word.encode('utf-8', 'ignore'), associationTy
