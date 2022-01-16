f3bd56afddba68cf934d752915efceac88994e86
hackgay <133792952+hackgay@users.noreply.github.com>
2022-01-16 16:20:13 +0000

    Fix licenses

--- associationtrainer.py
+++ associationtrainer.py
@@ -62,4 +62,5 @@ def find_associations(message):
                     # Check for words behind the word we're on
                     if sentence.length - word.index > 0:
                         # Check for words in front of the word we're on
-                        if sentence.
+                        if sentence.length != word.index + 1:
+ 
