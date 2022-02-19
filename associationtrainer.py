442883cf87da524f415556c98e3fcffa0e9694ab
hackgay <133792952+hackgay@users.noreply.github.com>
2022-02-19 08:50:11 +0000

    bump version to 0.7.1

--- associationtrainer.py
+++ associationtrainer.py
@@ -64,4 +64,5 @@ def find_associations(message):
                         # Check for words in front of the word we're on
                         if sentence.length != word.index + 1:
                             # Look for keywords
-                            if word.lemma == u'be' or word.le
+                            if word.lemma == u'be' or word.lemma == u'mean':
+                      
