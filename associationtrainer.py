2e695c55bf6ad8dccfcaba70ac8ac3fc71f09776
hackgay <133792952+hackgay@users.noreply.github.com>
2022-03-04 08:50:27 +0000

    Remove unnecessary classes

--- associationtrainer.py
+++ associationtrainer.py
@@ -69,4 +69,4 @@ def find_associations(message):
                                     if "ADJP" in sentence.words[word.index+1].chunk or "NP" in sentence.words[word.index+1].chunk:
                                         for nextWord in sentence.words[word.index+1:-1]:
                                             # NP + 'be' + ADJP >> NN HAS-PROPERTY JJ (milk is white >> milk HAS-PROPERTY white)
-                        
+                                            i
