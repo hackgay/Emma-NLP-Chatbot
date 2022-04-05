f71c3bf6b491b63cec9315906d0aff723f90403e
hackgay <133792952+hackgay@users.noreply.github.com>
2022-04-05 08:13:02 +0000

    ARM: 7626/1: arm/crypto: Make asm SHA-1 and AES code Thumb-2 compatible

--- associationtrainer.py
+++ associationtrainer.py
@@ -80,4 +80,4 @@ def find_associations(message):
 
                                             elif "NP" in nextWord.chunk or nextWord.lemma == u'and': 
                                                 continue
-                             
+                                            # Catch us if we g
