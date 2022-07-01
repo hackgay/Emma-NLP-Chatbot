c7016b546a4f5ca4dbadcb7fcde47885d5d1f6a6
hackgay <133792952+hackgay@users.noreply.github.com>
2022-07-01 07:50:58 +0000

    Added author

--- associationtrainer.py
+++ associationtrainer.py
@@ -100,4 +100,5 @@ def find_associations(message):
 
                             if word.partOfSpeech in misc.verbCodes:
                                 # VP containing RB + VB >> RB HAS-PROPERTY VB (It quickly moves >> quickly HAS-PROPERTY moves)
-                                if word.index != 0 and sentence.words[word.index-1].partOfSpeech in misc.adverbCod
+                                if word.index != 0 and sentence.words[word.index-1].partOfSpeech in misc.adverbCodes:
+                         
