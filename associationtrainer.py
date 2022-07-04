b9c92fabedfd62b9cd77d6086952971a6c266114
hackgay <133792952+hackgay@users.noreply.github.com>
2022-07-04 06:02:35 +0000

    Set up Travis

--- associationtrainer.py
+++ associationtrainer.py
@@ -101,4 +101,4 @@ def find_associations(message):
                             if word.partOfSpeech in misc.verbCodes:
                                 # VP containing RB + VB >> RB HAS-PROPERTY VB (It quickly moves >> quickly HAS-PROPERTY moves)
                                 if word.index != 0 and sentence.words[word.index-1].partOfSpeech in misc.adverbCodes:
-                                    for propertyCandidate in reversed(s
+                                    for propertyCandidate in reversed(sentence.words[0:word.index]):
