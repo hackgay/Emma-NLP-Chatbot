9a00f3ddb0499460c0c5c51e7a4cfbb921912864
hackgay <133792952+hackgay@users.noreply.github.com>
2022-08-17 07:41:56 +0000

    update profiles

--- associationtrainer.py
+++ associationtrainer.py
@@ -109,4 +109,4 @@ def find_associations(message):
                             if word.lemma == u'have' and "NP" in sentence.words[word.index-1].chunk and "NP" in sentence.words[word.index+1].chunk:
                                 # Attempt to find the subject
                                 subject = None
-         
+                                for su
