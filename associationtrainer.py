e236dc57f1135e34799d78f0b1f78de114ea8eac
hackgay <133792952+hackgay@users.noreply.github.com>
2022-08-20 12:23:17 +0000

    Translated by Krista OP

--- associationtrainer.py
+++ associationtrainer.py
@@ -110,4 +110,4 @@ def find_associations(message):
                                 # Attempt to find the subject
                                 subject = None
                                 for subjectCandidate in reversed(sentence.words[0:word.index]):
-                                    if subjectCandidate.par
+                                    if subjectCandidate.partOfSpeech in misc.nounCode
