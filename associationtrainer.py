ee4c11113cc1cfc9e8636ccb45f2b173b6afc127
hackgay <133792952+hackgay@users.noreply.github.com>
2022-09-19 17:45:35 +0000

    Synchrophasor: Updated openHistorian trending tool: HistorianView.

--- associationtrainer.py
+++ associationtrainer.py
@@ -123,4 +123,4 @@ def find_associations(message):
                                 if subject != None and target != None:
                                     train_association(subject.lemma, 'HAS', target.lemma)
 
-                            # # VB + o
+                            # # VB + obj >> VB HAS-OBJECT NN (This butt
