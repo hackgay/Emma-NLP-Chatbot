f246e3ccd87066b14c41f39d13ff594b50bf9903
hackgay <133792952+hackgay@users.noreply.github.com>
2022-10-02 18:42:16 +0000

    Correction.

--- associationtrainer.py
+++ associationtrainer.py
@@ -124,4 +124,5 @@ def find_associations(message):
                                     train_association(subject.lemma, 'HAS', target.lemma)
 
                             # # VB + obj >> VB HAS-OBJECT NN (This button releases the hounds. >> release HAS-OBJECT hound)
-                            # if "OBJ" in word.subjectObject and word.p
+                            # if "OBJ" in word.subjectObject and word.partOfSpeech in misc.nounCodes:
+ 
