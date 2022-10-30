4d4341178680e13283b77c281c812c2f218a2000
hackgay <133792952+hackgay@users.noreply.github.com>
2022-10-30 10:27:53 +0000

    update gh

--- emma.py
+++ emma.py
@@ -36,3 +36,28 @@ else:
         connection.cursor().executescript("""
         DROP TABLE IF EXISTS associationmodel;
         DROP TABLE IF EXISTS dictionary;
+        CREATE TABLE associationmodel(word TEXT, association_type TEXT, target TEXT, weight DOUBLE);
+        CREATE TABLE dictionary(word TEXT, part_of_speech TEXT, affinity DOUBLE)
+        """)
+    logging.debug("Association model created.")
+
+# Set up SQL
+connection = sql.connect('emma.db')
+connection.text_factory = str
+cursor = connection.cursor()
+
+# Dumb chrome
+print u"\n .ooooo.  ooo. .oo.  .oo.   ooo. .oo.  .oo.    .oooo.\nd88' \u006088b \u0060888P\"Y88bP\"Y88b  \u0060888P\"Y88bP\"Y88b  \u0060P  )88b\n888ooo888  888   888   888   888   888   888   .oP\"888\n888    .,  888   888   888   888   888   888  d8(  888\n\u0060Y8bod8P' o888o o888o o888o o888o o888o o888o \u0060Y888\"\"8o\n\n        ELECTRONIC MODEL of MAPPED ASSOCIATIONS\n                     Version " + misc.versionNumber + "\n"
+with connection:
+    cursor.execute("SELECT * FROM associationmodel")
+    associationModelItems = "{:,d}".format(len(cursor.fetchall()))
+    cursor.execute("SELECT * FROM dictionary")
+    dictionaryItems = "{:,d}".format(len(cursor.fetchall()))
+print "Database contains {0} associations for {1} words.".format(associationModelItems, dictionaryItems)
+
+# Check for and load the file containing the history of mood values or create it if it isn't there
+logging.info("Loading mood history...")
+if os.path.isfile('moodHistory.p'):
+    logging.debug("Mood history found!")
+    with open('moodHistory.p','rb') as moodFile: moodHistory = pickle.load(moodFile)
+    logging.debug("Mood history loaded!")
