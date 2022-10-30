4da6bdbd57305c9ff8c37ed43a6c75e675fceeb3
hackgay <133792952+hackgay@users.noreply.github.com>
2022-10-30 10:27:14 +0000

    vlan: Set correct source MAC address with TX VLAN offload enabled

--- emma.py
+++ emma.py
@@ -5,3 +5,34 @@ import pickle
 import logging
 import os
 import re
+import cgi
+import time
+
+import pattern.en
+import pattern.vector
+import sqlite3 as sql
+from mastodon import Mastodon, StreamListener
+
+import flags
+import pronouns
+import wordpatternfinder
+import associationtrainer
+import replybuilder
+import misc
+import apikeys
+
+# Setup stuff
+# Set up logging level (this should go in misc.py but eh)
+logging.root.setLevel(logging.INFO)
+
+# Pre-flight engine checks
+# Check for emma.db or create it if it isn't there
+logging.info("Checking for association model")
+if os.path.isfile('emma.db'):
+    logging.debug("Association model found!")
+else:
+    logging.warn("Association model not found! Creating")
+    with sql.connect('emma.db') as connection:
+        connection.cursor().executescript("""
+        DROP TABLE IF EXISTS associationmodel;
+        DROP TABLE IF EXISTS dictionary;
