769153bf80e25a5d66ff19844830dd3bb6c02666
hackgay <133792952+hackgay@users.noreply.github.com>
2022-12-27 09:00:18 +0000

    fixing spacing

--- misc.py
+++ misc.py
@@ -6,4 +6,6 @@ netspeak = {
     u'aight': [u'alright'],
     u'btw': [u'by', u'the', u'way'],
     u'cn': [u'can'],
-    u'gonna': [u'going', 
+    u'gonna': [u'going', u'to'],
+    u'im': [u'I\'m'],
+    u
