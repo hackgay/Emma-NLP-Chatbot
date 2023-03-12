adf531533d6384c8019610cebd326f774d9f5020
hackgay <133792952+hackgay@users.noreply.github.com>
2023-03-12 10:54:15 +0000

    rbtree: add postorder iteration functions

--- replybuilder.py
+++ replybuilder.py
@@ -54,3 +54,29 @@ def weighted_roll(choices):
     dieSeed = 0
     for choice in choices:
         dieSeed += choice[0]
+    dieResult = random.uniform(0, dieSeed)
+
+    for choice in choices:
+        dieResult -= choice[0]
+        if dieResult <= 0:
+            return choice[1]
+
+class Association:
+    def __init__(self, word, associationType, target, weight):
+        self.word = word
+        self.target = target
+        self.associationType = associationType
+        self.weight = weight
+
+def find_associations(keyword):
+    """Finds associations in our association model for given keywords"""
+    logging.debug("Finding associations for {0}...".format(keyword)) 
+    associations = []
+    with connection:
+        cursor.execute('SELECT * FROM associationmodel WHERE word = ? OR target = ?;', (keyword, keyword))
+        SQLReturn = cursor.fetchall()
+        for row in SQLReturn:
+            associations.append(Association(row[0], row[1], row[2], row[3]))
+    return associations
+
+def find_part_of_speech(keyword):
