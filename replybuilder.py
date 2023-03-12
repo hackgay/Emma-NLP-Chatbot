0c5a237dc723ba9a0e9323da6c48a28799498f83
hackgay <133792952+hackgay@users.noreply.github.com>
2023-03-12 09:47:48 +0000

    Rename spec file

--- replybuilder.py
+++ replybuilder.py
@@ -27,3 +27,30 @@ class SBBWord:
         with connection:
             cursor.execute('SELECT part_of_speech FROM dictionary WHERE word = ?;', (self.word,))
             SQLReturn = cursor.fetchall()
+            self.partOfSpeech = SQLReturn[0]
+
+class SBBHaveHas:
+    def __init__(self):
+        pass
+
+class SBBIsAre:
+    def __init__(self):
+        pass
+
+class SBBArticle:
+    def __init__(self):
+        pass
+
+class SBBConjunction:
+    def __init__(self):
+        pass
+
+class SBBPunctuation:
+    def __init__(self):
+        pass
+
+def weighted_roll(choices):
+    """Takes a list of (weight, option) tuples and makes a weighted die roll"""
+    dieSeed = 0
+    for choice in choices:
+        dieSeed += choice[0]
