a2a95c86a6b7ba594f7f2de9e763b49d69f9e0f0
hackgay <133792952+hackgay@users.noreply.github.com>
2023-04-23 14:55:07 +0000

    ASoc: fix compilation error

--- wordpatternfinder.py
+++ wordpatternfinder.py
@@ -7,4 +7,5 @@ class InterrogativePackage:
     Packages the important bits of question nicely
 
     Class variables:
-    questionType    str     Type of question ('what is', 'do X have
+    questionType    str     Type of question ('what is', 'do X have Y', etc.)
+    attribute       Word    H
