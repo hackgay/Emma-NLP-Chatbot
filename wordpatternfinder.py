4ff420b536e058947ba30ea135818eeccec89f8c
hackgay <133792952+hackgay@users.noreply.github.com>
2023-05-06 19:19:51 +0000

    Fixes...

--- wordpatternfinder.py
+++ wordpatternfinder.py
@@ -9,4 +9,4 @@ class InterrogativePackage:
     Class variables:
     questionType    str     Type of question ('what is', 'do X have Y', etc.)
     attribute       Word    Half of the important question bits ('what is the color of the sky?' <- 'color')
-    subject         Word    The oth
+    subject         Word    The other half ('what is the color of
