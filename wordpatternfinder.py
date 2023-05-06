8fa75d2b5c47870f99a9c0f40c5c021940715cea
hackgay <133792952+hackgay@users.noreply.github.com>
2023-05-06 08:52:23 +0000

    added style

--- wordpatternfinder.py
+++ wordpatternfinder.py
@@ -8,4 +8,4 @@ class InterrogativePackage:
 
     Class variables:
     questionType    str     Type of question ('what is', 'do X have Y', etc.)
-    attribute       Word    Half of the important question bit
+    attribute       Word    Half of the important question bits ('what is the c
