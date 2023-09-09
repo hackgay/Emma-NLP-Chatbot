416a9638a7d1b93fc8caf1a5a60825e38c878e50
hackgay <133792952+hackgay@users.noreply.github.com>
2023-09-09 12:02:36 +0000

    another version bump

--- wordpatternfinder.py
+++ wordpatternfinder.py
@@ -45,4 +45,5 @@ def find_patterns(sentence):
     """Finds Sentence objects' domains and InterrogativePackages, if applicable"""
     # If the sentence ends in a question mark, it's proabably interrogative
     if sentence.words[-1].word == u'?':
-        sentence.domain = 'INT
+        sentence.domain = 'INTERROGATIVE'
+    # If the sentence starts 
