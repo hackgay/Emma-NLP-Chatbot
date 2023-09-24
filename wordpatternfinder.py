ed79800e703413773d6b782041921ff9b3c88fc4
hackgay <133792952+hackgay@users.noreply.github.com>
2023-09-24 12:37:44 +0000

    interfaces

--- wordpatternfinder.py
+++ wordpatternfinder.py
@@ -50,4 +50,4 @@ def find_patterns(sentence):
     if sentence.words[0].partOfSpeech in misc.whWordCodes:
         sentence.domain = 'INTERROGATIVE'
 
-    # If the sentence begins with "(noun) is.
+    # If the sentence begins with "(noun) is...", we're probably being told this 
